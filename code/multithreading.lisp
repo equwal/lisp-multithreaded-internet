e5e0ea90c2f082b1a4c3601615690287985d6f0c
john <john@linux-srhk.suse>
2018-03-26 01:58:56 -0300

    Bugs.

--- code/multithreading.lisp
+++ code/multithreading.lisp
@@ -11,19 +11,6 @@
     (if (stringp result)
 	result
 	(drakma::octets-to-string result))))
-
-(defmacro mvbind (vars value-form &body body)
-  `(multiple-value-bind ,vars
-       ,value-form
-     ,@body))
-(uiop::chdir (toplevel-dir))
-(defvar *log-file* (get-file-in-toplevel "log" "txt"))
-(defun log-result (code-result)
-  (with-open-file (s *log-file*
-		     :if-does-not-exist :create
-		     :if-exists :append
-		     :direction :output)
-    (princ code-result s)))
 (defun getpage (uri)
   "Uri should be 'http(s)://domainname.blah.toplevel/blah/blah/blah'"
   (drakma-no-octets uri))
